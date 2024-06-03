a5a0bd4e5c356ed3ed453c6046d2fed6651febca
kei224022 <ishikawa224022@protonmail.com>
2024-06-03 14:37:22 +0900

    Initial Commit

---
+++ SideMenuTest/SideMenuTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuTestApp.swift
+//  SideMenuTest
+//
+//  Created by member on 2024/06/03.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
